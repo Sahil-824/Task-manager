b569c4d0d4429263993207382db0c83bfb1c6ff2
Sahil-824 <129472809+Sahil-824@users.noreply.github.com>
2024-09-25 15:18:53 +0530

    Update axiosClient.js

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -3,7 +3,7 @@ import { KEY_ACCESS_TOKEN, getItem } from "./localStorageManager";
 
 import toast from "react-hot-toast";
 
-const baseURL = "http://localhost:4000";
+const baseURL = "https://task-manager-backend-cuia.onrender.com";
 
 export const axiosClient = axios.create({
   baseURL,
